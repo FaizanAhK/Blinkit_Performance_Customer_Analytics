bdf1fc7479797ec6be71c73c283a5c4033948832
Faizan Khan <94823280+FaizanAhK@users.noreply.github.com>
2025-05-07 15:37:41 +0530

    Add files via upload

---
+++ blinkit_data_analysis.sql
@@ -0,0 +1,21 @@
+SELECT * FROM blinkit_data
+
+SELECT COUNT(*) FROM blinkit_data; -- Counts the number of row entries
+
+UPDATE blinkit_data                                         /*updated in Item_Fat_Content column : LF, low fat to Low Fat and
+                                                              reg to Regular*/
+SET Item_Fat_Content = 
+CASE
+WHEN Item_Fat_Content IN ('LF', 'low fat') THEN 'Low Fat'
+WHEN Item_Fat_Content = 'reg' THEN 'Regular'
+ELSE Item_Fat_Content
+END
+
+SELECT DISTINCT(Item_Fat_Content) FROM blinkit_data       -- Selecting distinct items
+
+SELECT SUM(Sales) AS Total_Sales 
+FROM blinkit_data                  -- Calculating total sales from the 'Sales' column
+
+SELECT CAST(SUM(Sales)/1000000 AS DECIMAL(10,2)) AS Total_Sales_Millions 
+FROM blinkit_data            -- Calculating Total Sales in Million by dividing it by 1000000.
+
